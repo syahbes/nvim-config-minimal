b62301ff7636c1d59488bffa49d714114d681d17
Shlomi Y. <syahbes@gmail.com>
2025-12-16 16:29:48 +0200

    tele

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,10 +9,10 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-        -- new
         vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Recent files' })
         vim.keymap.set('n', '<C-P>', builtin.git_files, { desc = 'Git files' })
+        vim.keymap.set('n', "'", builtin.marks, { desc = 'Telescope marks' })
+        
     end
 }
 
